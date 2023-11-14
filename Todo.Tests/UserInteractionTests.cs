94827ba22924390b7d1da9e34f36a54a4b4b8f8d
Robin <loeveet@gmail.com>
2023-11-14 22:07:31 +0100

    ...

--- Todo.Tests/UserInteractionTests.cs
+++ Todo.Tests/UserInteractionTests.cs
@@ -11,164 +11,164 @@ namespace Todo.Tests
 {
     public class UserInteractionTests
     {
-        [Fact]
-        public void GetInputWorksWhenUserInputsString()
-        {
-            // Arrange
-            var mock = new Mock<IConsoleWrapper>();
-            var expected = "Daniel";
-            mock.Setup(x => x.ReadLine()).Returns(expected);
-            var sut = new UserInteractionWrapper(mock.Object);
+        //        [Fact]
+        //        public void GetInputWorksWhenUserInputsString()
+        //        {
+        //            // Arrange
+        //            var mock = new Mock<IConsoleWrapper>();
+        //            var expected = "Daniel";
+        //            mock.Setup(x => x.ReadLine()).Returns(expected);
+        //            var sut = new UserInteractionWrapper(mock.Object);
 
-            // Act
-            var actual = sut.GetStringInput("Vad heter du?");
+        //            // Act
+        //            var actual = sut.GetStringInput("Vad heter du?");
 
-            // Assert
-            Assert.Equal(expected, actual);
-        }
+        //            // Assert
+        //            Assert.Equal(expected, actual);
+        //        }
 
-        [Fact]
-        public void GetInputKeepsTryingOnEmpty()
-        {
-            // Arrange
-            var mock = new Mock<IConsoleWrapper>();
-            var expected = "Daniel";
-            mock.SetupSequence(x => x.ReadLine())
-                .Returns("")
-                .Returns("")
-                .Returns("")
-                .Returns(expected);
-            var sut = new UserInteractionWrapper(mock.Object);
+        //        [Fact]
+        //        public void GetInputKeepsTryingOnEmpty()
+        //        {
+        //            // Arrange
+        //            var mock = new Mock<IConsoleWrapper>();
+        //            var expected = "Daniel";
+        //            mock.SetupSequence(x => x.ReadLine())
+        //                .Returns("")
+        //                .Returns("")
+        //                .Returns("")
+        //                .Returns(expected);
+        //            var sut = new UserInteractionWrapper(mock.Object);
 
-            // Act
-            var actual = sut.GetStringInput("Vad heter du?");
+        //            // Act
+        //            var actual = sut.GetStringInput("Vad heter du?");
 
-            // Assert
-            Assert.Equal(expected, actual);
-            mock.Verify(x => x.ReadLine(), Times.Exactly(4));
-        }
+        //            // Assert
+        //            Assert.Equal(expected, actual);
+        //            mock.Verify(x => x.ReadLine(), Times.Exactly(4));
+        //        }
 
-        //[Fact]
-        //public void Test_UserInteraction_GetUserInput_Sequential()
-        //{
-        //    // Arrange
-        //    var mock = new Mock<IConsoleWrapper>();
+        //        //[Fact]
+        //        //public void Test_UserInteraction_GetUserInput_Sequential()
+        //        //{
+        //        //    // Arrange
+        //        //    var mock = new Mock<IConsoleWrapper>();
 
-        //    var sequence = new MockSequence();
+        //        //    var sequence = new MockSequence();
 
-        //    mock.Setup(x =>
-        //        x.ReadLine("Ange titel: ")).Returns("Vattna blommorna");
+        //        //    mock.Setup(x =>
+        //        //        x.ReadLine("Ange titel: ")).Returns("Vattna blommorna");
 
 
-        //    var sut = new UserInteractionWrapper<mock.Object>();
+        //        //    var sut = new UserInteractionWrapper<mock.Object>();
 
-        //    //Act
-        //    var result1 = sut.GetInput("Ange titel: ");
-        //    var result2 = sut.GetInput("Ange beskrivning: ");
+        //        //    //Act
+        //        //    var result1 = sut.GetInput("Ange titel: ");
+        //        //    var result2 = sut.GetInput("Ange beskrivning: ");
 
-        //    // Assert
-        //    Assert.Equal("Vattna blommorna", result1);
-        //    Assert.Equal("Alla blommor utom de i köket", result2);
-        //}
+        //        //    // Assert
+        //        //    Assert.Equal("Vattna blommorna", result1);
+        //        //    Assert.Equal("Alla blommor utom de i köket", result2);
+        //        //}
 
-        [Theory]
-        [InlineData("Daniel", "Vad heter du?")] 
-        [InlineData("", "Vad heter du?", typeof(ArgumentNullException))] 
-        [InlineData(null, "Vad heter du?", typeof(ArgumentNullException))]
-        public void GetInputHandlesDifferentScenarios(string expected, string instruction, Type expectedExceptionType = null)
-        {
-            // Arrange
-            var mock = new Mock<IConsoleWrapper>();
-            mock.Setup(x => x.ReadLine())
-                .Returns(expected);
-            var sut = new UserInteractionWrapper(mock.Object);
+        //        [Theory]
+        //        [InlineData("Daniel", "Vad heter du?")] 
+        //        [InlineData("", "Vad heter du?", typeof(ArgumentNullException))] 
+        //        [InlineData(null, "Vad heter du?", typeof(ArgumentNullException))]
+        //        public void GetInputHandlesDifferentScenarios(string expected, string instruction, Type expectedExceptionType = null)
+        //        {
+        //            // Arrange
+        //            var mock = new Mock<IConsoleWrapper>();
+        //            mock.Setup(x => x.ReadLine())
+        //                .Returns(expected);
+        //            var sut = new UserInteractionWrapper(mock.Object);
 
-            // Act och Assert
-            if (expectedExceptionType == null)
-            {
-                var acutal = sut.GetStringInput(instruction);
-                Assert.Equal(expected, acutal);
-            }
-            else
-            {
-                Assert.Throws(expectedExceptionType, () => sut.GetStringInput(instruction));
-            }
-        }
+        //            // Act och Assert
+        //            if (expectedExceptionType == null)
+        //            {
+        //                var acutal = sut.GetStringInput(instruction);
+        //                Assert.Equal(expected, acutal);
+        //            }
+        //            else
+        //            {
+        //                Assert.Throws(expectedExceptionType, () => sut.GetStringInput(instruction));
+        //            }
+        //        }
 
-        [Fact]
-        public void GetTodoItemByIndex_IndexWithinRange_ReturnsTodoItem()
-        {
-            // Arrange
-            var todoListStorageMock = new Mock<IStorage>();
-            var todoListService = new TodoList(todoListStorageMock.Object);
+        //        [Fact]
+        //        public void GetTodoItemByIndex_IndexWithinRange_ReturnsTodoItem()
+        //        {
+        //            // Arrange
+        //            var todoListStorageMock = new Mock<IStorage>();
+        //            var todoListService = new TodoList(todoListStorageMock.Object);
 
-            todoListStorageMock.Setup(x => x.GetAllTodos()).Returns(new List<TodoItem>
-         {
-             new TodoItem {  },
-             new TodoItem {  },
-             new TodoItem {  }
-         });
+        //            todoListStorageMock.Setup(x => x.GetAllTodos()).Returns(new List<TodoItem>
+        //         {
+        //             new TodoItem {  },
+        //             new TodoItem {  },
+        //             new TodoItem {  }
+        //         });
 
-            // Act
-            var result = todoListService.GetTodoItemByIndex(2);
+        //            // Act
+        //            var result = todoListService.GetTodoItemByIndex(2);
 
-            // Assert
-            Assert.NotNull(result);
-        }
+        //            // Assert
+        //            Assert.NotNull(result);
+        //        }
 
-        [Fact]
-        public void GetTodoItemByIndex_IndexOutOfRange_ThrowsException()
-        {
-            // Arrange
-            var todoListStorageMock = new Mock<IStorage>();
-            var todoListService = new TodoList(todoListStorageMock.Object);
+        //        [Fact]
+        //        public void GetTodoItemByIndex_IndexOutOfRange_ThrowsException()
+        //        {
+        //            // Arrange
+        //            var todoListStorageMock = new Mock<IStorage>();
+        //            var todoListService = new TodoList(todoListStorageMock.Object);
 
-            todoListStorageMock.Setup(x => x.GetAllTodos()).Returns(new List<TodoItem>
-     {
-         new TodoItem {  },
-         new TodoItem {  },
-         new TodoItem {  }
-     });
+        //            todoListStorageMock.Setup(x => x.GetAllTodos()).Returns(new List<TodoItem>
+        //     {
+        //         new TodoItem {  },
+        //         new TodoItem {  },
+        //         new TodoItem {  }
+        //     });
 
-            // Act and Assert
-            Assert.Throws<InvalidOperationException>(() => todoListService.GetTodoItemByIndex(5));
-        }
-        [Fact]
-        public void GetInputWorksWhenUserInputsInt()
-        {
-            // Arrange
-            var mock = new Mock<IConsoleWrapper>();
-            var expected = "42";
-            mock.SetupSequence(x => x.ReadLine())
-                .Returns(expected);
-            var sut = new UserInteractionWrapper(mock.Object);
+        //            // Act and Assert
+        //            Assert.Throws<InvalidOperationException>(() => todoListService.GetTodoItemByIndex(5));
+        //        }
+        //        [Fact]
+        //        public void GetInputWorksWhenUserInputsInt()
+        //        {
+        //            // Arrange
+        //            var mock = new Mock<IConsoleWrapper>();
+        //            var expected = "42";
+        //            mock.SetupSequence(x => x.ReadLine())
+        //                .Returns(expected);
+        //            var sut = new UserInteractionWrapper(mock.Object);
 
-            // Act
-            var actual = sut.GetIntInput("Ange ett heltal:");
+        //            // Act
+        //            var actual = sut.GetIntInput("Ange ett heltal:");
 
-            // Assert
-            Assert.Equal(42, actual);
-        }
+        //            // Assert
+        //            Assert.Equal(42, actual);
+        //        }
 
-        [Fact]
-        public void GetInputKeepsTryingOnNonValidInteger()
-        {
-            // Arrange
-            var mock = new Mock<IConsoleWrapper>();
-            var expected = "42";
-            mock.SetupSequence(x => x.ReadLine())
-                .Returns("inte ett heltal")
-                .Returns("inte heller ett heltal")
-                .Returns("")
-                .Returns(expected);
-            var sut = new UserInteractionWrapper(mock.Object);
+        //        [Fact]
+        //        public void GetInputKeepsTryingOnNonValidInteger()
+        //        {
+        //            // Arrange
+        //            var mock = new Mock<IConsoleWrapper>();
+        //            var expected = "42";
+        //            mock.SetupSequence(x => x.ReadLine())
+        //                .Returns("inte ett heltal")
+        //                .Returns("inte heller ett heltal")
+        //                .Returns("")
+        //                .Returns(expected);
+        //            var sut = new UserInteractionWrapper(mock.Object);
 
-            // Act
-            var actual = sut.GetIntInput("Ange ett heltal:");
+        //            // Act
+        //            var actual = sut.GetIntInput("Ange ett heltal:");
 
-            // Assert
-            Assert.Equal(42, actual);
-            mock.Verify(x => x.ReadLine(), Times.Exactly(4));
-        }
+        //            // Assert
+        //            Assert.Equal(42, actual);
+        //            mock.Verify(x => x.ReadLine(), Times.Exactly(4));
+        //        }
     }
 }
